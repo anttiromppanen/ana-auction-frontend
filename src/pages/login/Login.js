42074f00437110f2dc32e59f904fced7c7d4866f
Antti Romppanen <romppanen.antti@gmail.com>
2022-04-14 00:18:55 +0300

    register- & login-forms skeleton

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,13 +1,37 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Button from '@mui/material/Button';
 
 import DrawerHeader from '../../components/DrawerHeader';
 
 const Login = () => {
   return (
-    <Box component="main" sx={{ marginLeft: '2rem', marginRight: '2rem' }}>
+    <Box 
+      component="main"
+      sx={{ marginLeft: '2rem', marginRight: '2rem' }}
+    >
       <DrawerHeader />
-      <Typography variant="h3">LOG IN</Typography>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+        <div>
+          <Typography variant="h3" gutterBottom>LOG IN</Typography>
+          <TextField
+            label="With normal TextField"
+            id="outlined-start-adornment"
+            sx={{ m: 1 }}
+          />
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            label="Password"
+          />
+        </FormControl>
+        <Button variant="contained" color="primary" type="submit">LOG IN</Button>
+        </div>
+      </Box>
     </Box>
 )};
 
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -4,9 +4,7 @@ import Typography from '@mui/material/Typography';
 
 const Register = () => {
   return (
-    <Box component="main" sx={{ marginLeft: '2rem', marginRight: '2rem' }}>
-      <Typography variant="h3">REGISTER</Typography>
-    </Box>
+    <div>Hello</div>
   )
 }
 
